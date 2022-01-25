09e8c39d483a2f70de9990ad08eedd34e54cd4e6
joseph-106 <sjjoseph106@gmail.com>
2022-01-25 11:55:35 +0900

    LV1_직사각형별찍기

---
+++ programmers-js/LV1_직사각형별찍기.js
@@ -0,0 +1,10 @@
+//연습문제
+
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+  const n = data.split(" ");
+  const a = Number(n[0]),
+    b = Number(n[1]);
+  // repeat()로 반복, 이스케이프 시퀀스로 줄바꿈
+  console.log(("*".repeat(a) + "\n").repeat(b));
+});
