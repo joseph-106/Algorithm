5ee37c0998df4821cf9a15b58c6d7feb19f8c61b
unknown <sjjoseph106@gmail.com>
2022-01-29 14:30:16 +0900

    Comment

--- programmers-js/LV1_하샤드수.js
+++ programmers-js/LV1_하샤드수.js
@@ -1,8 +1,10 @@
+// 연습문제
+
 function solution(x) {
   // x를 숫자 배열로 만들고 reduce로 총합 구하기
   var array = x.toString().split("").map(Number);
   var sum = array.reduce((acc, cur) => {
     return acc + cur;
   });
-  return x % sum == 0 ? true : false;
+  return x % sum == 0;
 }
