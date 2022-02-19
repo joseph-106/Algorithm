ae837b647751363883dfc772d09b278d256d6d5a
unknown <sjjoseph106@gmail.com>
2022-02-19 13:56:11 +0900

    Refactor

--- programmers-js/연습문제/LV1_두정수사이의합.js
+++ programmers-js/연습문제/LV1_두정수사이의합.js
@@ -1,13 +1,13 @@
 // 연습문제
 
 function solution(a, b) {
-  const arr = [];
+  let result = 0;
   // Math.abs로 절대값 반환
   for (let i = 0; i < Math.abs(a - b) + 1; i++) {
-    a > b ? arr.push(b + i) : arr.push(a + i);
+    result += a > b ? b + i : a + i;
   }
-  return arr.reduce((acc, cur) => acc + cur);
+  return result;
 
-  // 배열 안쓰고 풀기
+  // 가우스 덧셈
   // return (a+b)*(Math.abs(b-a)+1)/2;
 }
