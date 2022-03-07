2f1ba98e4700a56a9505f53aed394b666d231581
unknown <sjjoseph106@gmail.com>
2022-03-07 11:41:14 +0900

    Comment

--- programmers-js/LV1_실패율.js
+++ programmers-js/LV1_실패율.js
@@ -17,3 +17,17 @@ function solution(N, stages) {
   arr.forEach((e) => answer.push(e.stage));
   return answer;
 }
+
+// 객체 안쓰고 풀기
+// function solution(N, stages) {
+//   let result = [];
+//   for (let i = 1; i <= N; i++) {
+//     result.push([
+//       i,
+//       stages.filter((e) => e === i).length /
+//         stages.filter((e) => e >= i).length,
+//     ]);
+//   }
+//   result.sort((a, b) => b[1] - a[1]);
+//   return result.map((e) => e[0]);
+// }
