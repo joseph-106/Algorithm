206ba186cf4ba34f9f7a8f8ab0889e1ef48b2bbe
unknown <sjjoseph106@gmail.com>
2022-03-15 08:57:20 +0900

    LV1_소수만들기

---
+++ programmers-js/LV1_소수만들기.js
@@ -0,0 +1,25 @@
+// Summer/Winter Coding(~2018)
+
+// 소수 판별 함수
+const isPrime = (n) => {
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+function solution(nums) {
+  let answer = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if (isPrime(nums[i] + nums[j] + nums[k])) {
+          answer++;
+        }
+      }
+    }
+  }
+  return answer;
+}
