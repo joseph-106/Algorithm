9eae2d8ad1bf193b9cedb213b1d04123e3f0bc38
unknown <sjjoseph106@gmail.com>
2022-03-26 17:34:33 +0900

    3_연필개수

---
+++ inflearn-js/기본문제/3_연필개수.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  // 소수점 올림
+  return Math.ceil(n / 12);
+}
+
+console.log(solution(178));
+
+// function solution(n) {
+//   let answer;
+//   answer = Math.ceil(n / 12);
+//   return answer;
+// }
