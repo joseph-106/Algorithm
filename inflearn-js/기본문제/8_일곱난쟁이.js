8aa9e10f8fd11f977835c42293c5b08f43fa7716
unknown <sjjoseph106@gmail.com>
2022-03-29 09:46:03 +0900

    8_일곱난쟁이

---
+++ inflearn-js/기본문제/8_일곱난쟁이.js
@@ -0,0 +1,29 @@
+function solution(arr) {
+  const over = arr.reduce((acc, cur) => acc + cur) - 100;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === over) {
+        arr.splice(i, 1);
+        arr.splice(j - 1, 1);
+        return arr;
+      }
+    }
+  }
+}
+
+let arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
+console.log(solution(arr));
+
+// function solution(arr) {
+//   let answer = arr;
+//   let sum = answer.reduce((a, b) => a + b, 0);
+//   for (let i = 0; i < 8; i++) {
+//     for (let j = i + 1; j < 9; j++) {
+//       if (sum - (answer[i] + answer[j]) == 100) {
+//         answer.splice(j, 1);
+//         answer.splice(i, 1);
+//       }
+//     }
+//   }
+//   return answer;
+// }
