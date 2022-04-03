a35825a4b7d09c14e0f80ba2b51553705fe547db
unknown <sjjoseph106@gmail.com>
2022-04-03 12:28:19 +0900

    13_대소문자변환

---
+++ inflearn-js/기본문제/13_대소문자변환.js
@@ -0,0 +1,14 @@
+function solution(s) {
+  return s.toUpperCase();
+}
+
+console.log(solution("StuDY"));
+
+// function solution(s) {
+//   let answer = "";
+//   for (let x of s) {
+//     if (x === x.toUpperCase()) answer += x.toLowerCase();
+//     else answer += x.toUpperCase();
+//   }
+//   return answer;
+// }
