a1d11f2e2d06859bc4fc966f133c77fc07fe6e21
unknown <sjjoseph106@gmail.com>
2022-04-22 02:34:28 +0900

    Refactor

--- inflearn-js/브루트포스/2_뒤집은소수.js
+++ inflearn-js/브루트포스/2_뒤집은소수.js
@@ -1,11 +1,10 @@
 const isPrime = (n) => {
   if (n === 1) return false;
-  for (let i = 2; i < Math.floor(Math.sqrt(n)); i++) {
+  for (let i = 2; i < parseInt(Math.sqrt(n)); i++) {
     if (n % i === 0) return false;
   }
   return true;
 };
-
 function solution(arr) {
   const primeArr = [];
   for (let i = 0; i < arr.length; i++) {
