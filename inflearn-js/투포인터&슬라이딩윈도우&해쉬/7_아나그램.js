2e00308b35707da289ea0ee342def1a250425ffb
unknown <sjjoseph106@gmail.com>
2022-05-01 13:09:39 +0900

    7_아나그램

---
+++ inflearn-js/투포인터&슬라이딩윈도우&해쉬/7_아나그램.js
@@ -0,0 +1,23 @@
+function solution(str1, str2) {
+  return str1.split("").sort().join("") === str2.split("").sort().join("")
+    ? "YES"
+    : "No";
+}
+
+let a = "AbaAeCe";
+let b = "baeeACA";
+console.log(solution(a, b));
+
+/* function solution(str1, str2) {
+  let answer = "YES";
+  let sH = new Map();
+  for (let x of str1) {
+    if (sH.has(x)) sH.set(x, sH.get(x) + 1);
+    else sH.set(x, 1);
+  }
+  for (let x of str2) {
+    if (!sH.has(x) || sH.get(x) == 0) return "NO";
+    sH.set(x, sH.get(x) - 1);
+  }
+  return answer;
+} */
