403f96e99a4f84343fe1bdb78f13ec267297e430
unknown <sjjoseph106@gmail.com>
2022-05-13 15:12:56 +0900

    4_삽입정렬

---
+++ inflearn-js/정렬&그리디&이분검색/4_삽입정렬.js
@@ -0,0 +1,40 @@
+// 삽입 정렬 알고리즘
+function solution(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = i; j > 0; j--) {
+      if (arr[j] < arr[j - 1]) [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
+    }
+  }
+  return arr;
+}
+
+let arr = [11, 7, 5, 6, 10, 9];
+console.log(solution(arr));
+
+/* function solution(arr) {
+  let answer = arr;
+  for (let i = 0; i < arr.length; i++) {
+    let tmp = arr[i],
+      j;
+    for (j = i - 1; j >= 0; j--) {
+      if (arr[j] > tmp) arr[j + 1] = arr[j];
+      else break;
+    }
+    arr[j + 1] = tmp;
+  }
+  return answer;
+} */
+
+/* function solution(arr) {
+  let answer = [];
+  answer.push(arr[0]);
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < answer.length; j++) {
+      if (arr[i] < answer[j]) {
+        answer.splice(j, 0, arr[i]);
+        break;
+      }
+    }
+  }
+  return answer;
+} */
