b41d6f440f5daee6930a74cf9dc79dcb7f58ae8e
unknown <sjjoseph106@gmail.com>
2022-05-16 00:55:04 +0900

    7_좌표정렬

---
+++ inflearn-js/정렬&그리디&이분검색/7_좌표정렬.js
@@ -0,0 +1,21 @@
+function solution(arr) {
+  return arr.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]));
+}
+
+let arr = [
+  [2, 7],
+  [1, 3],
+  [1, 2],
+  [2, 5],
+  [3, 6],
+];
+console.log(solution(arr));
+
+/* function solution(arr) {
+  let answer = arr;
+  arr.sort((a, b) => {
+    if (a[0] === b[0]) return a[1] - b[1];
+    else return a[0] - b[0];
+  });
+  return answer;
+} */
