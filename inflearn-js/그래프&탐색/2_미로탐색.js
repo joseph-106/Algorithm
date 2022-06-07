eeab472a985fbfee2558b47b5346e1366f4258d4
unknown <sjjoseph106@gmail.com>
2022-06-07 10:34:05 +0900

    2_미로탐색

---
+++ inflearn-js/그래프&탐색/2_미로탐색.js
@@ -0,0 +1,53 @@
+function solution(board) {
+  let count = 0;
+  const dx = [1, 0, -1, 0];
+  const dy = [0, 1, 0, -1];
+  const recursive = (x, y) => {
+    if (x === 6 && y === 6) count++;
+    for (let i = 0; i < 4; i++) {
+      const nx = x + dx[i];
+      const ny = y + dy[i];
+      if (board[nx][ny] === 0 && nx >= 0 && nx <= 6 && ny >= 0 && ny <= 6) {
+        board[x][y] = 1;
+        recursive(nx, ny);
+        board[x][y] = 0;
+      }
+    }
+  };
+  recursive(0, 0);
+  return count;
+}
+
+let arr = [
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 1, 1, 0],
+  [0, 0, 0, 1, 0, 0, 0],
+  [1, 1, 0, 1, 0, 1, 1],
+  [1, 1, 0, 0, 0, 0, 1],
+  [1, 1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 0, 0, 0],
+];
+console.log(solution(arr));
+
+/* function solution(board) {
+  let answer = 0;
+  let dx = [-1, 0, 1, 0];
+  let dy = [0, 1, 0, -1];
+  function DFS(x, y) {
+    if (x === 6 && y === 6) answer++;
+    else {
+      for (let k = 0; k < 4; k++) {
+        let nx = x + dx[k];
+        let ny = y + dy[k];
+        if (nx >= 0 && nx <= 6 && ny >= 0 && ny <= 6 && board[nx][ny] === 0) {
+          board[nx][ny] = 1;
+          DFS(nx, ny);
+          board[nx][ny] = 0;
+        }
+      }
+    }
+  }
+  board[0][0] = 1;
+  DFS(0, 0);
+  return answer;
+} */
