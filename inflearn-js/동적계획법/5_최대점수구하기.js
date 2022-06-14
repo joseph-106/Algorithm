29afdacc15bec0510b24794c06bab0f6b1ecb0fa
unknown <sjjoseph106@gmail.com>
2022-06-15 00:26:34 +0900

    5_최대점수구하기

---
+++ inflearn-js/동적계획법/5_최대점수구하기.js
@@ -0,0 +1,34 @@
+function solution(m, arr) {
+  let maxPoint = Number.MIN_SAFE_INTEGER;
+  const recursive = (time, point, numArr) => {
+    for (let i = 0; i < arr.length; i++)
+      if (!numArr.includes(i) && time + arr[i][1] <= m)
+        recursive(time + arr[i][1], point + arr[i][0], [...numArr, i]);
+    if (point > maxPoint) maxPoint = point;
+  };
+  recursive(0, 0, []);
+  return maxPoint;
+}
+
+let arr = [
+  [10, 5],
+  [25, 12],
+  [15, 8],
+  [6, 3],
+  [7, 4],
+];
+console.log(solution(20, arr));
+
+/* function solution(m, arr) {
+  let answer = 0;
+  let dy = Array.from({ length: m + 1 }, () => 0);
+  for (let i = 0; i < arr.length; i++) {
+    let ps = arr[i][0];
+    let pt = arr[i][1];
+    for (let j = m; j >= pt; j--) {
+      dy[j] = Math.max(dy[j], dy[j - pt] + ps);
+    }
+  }
+  answer = dy[m];
+  return answer;
+} */
