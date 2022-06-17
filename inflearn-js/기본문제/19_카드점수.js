dfffa00cf6cf9253ee1dc575a6879ef910036a94
unknown <sjjoseph106@gmail.com>
2022-06-17 19:26:49 +0900

    19_카드점수

---
+++ inflearn-js/기본문제/19_카드점수.js
@@ -0,0 +1,30 @@
+function solution(nums, k) {
+  let max = Number.MIN_VALUE;
+  const recursive = (left, right, sum) => {
+    if (left + right < k) {
+      recursive(left + 1, right, sum + nums[left]);
+      recursive(left, right + 1, sum + nums[nums.length - right - 1]);
+    } else if (sum > max) max = sum;
+  };
+  recursive(0, 0, 0);
+  return max;
+}
+
+console.log(solution([3, 2, 5, 6, 7, 1], 3)); //14
+console.log(solution([3, 1, 4, 5, 4, 1, 2, 5], 5)); //18
+console.log(solution([6, 7, 1, 3, 1, 4, 3, 1, 1, 5, 4, 1, 2, 5], 10)); //35
+
+/* function solution(nums, k) {
+  let answer = nums.reduce((a, b) => a + b);
+  let sum = 0;
+  let remain = nums.length - k;
+  for (let i = 0; i < remain; i++) {
+    sum += nums[i];
+  }
+  let Nmin = sum;
+  for (let i = remain; i < nums.length; i++) {
+    sum += nums[i] - nums[i - remain];
+    Nmin = Math.min(Nmin, sum);
+  }
+  return answer - Nmin;
+} */
