325d908e0e9548dbd6000a9b0486a18e583e2143
unknown <sjjoseph106@gmail.com>
2022-11-01 13:35:18 +0900

    Refactor

--- programmers-js/DFS&BFS/LV3_네트워크.js
+++ programmers-js/DFS&BFS/LV3_네트워크.js
@@ -1,17 +1,17 @@
 function solution(n, computers) {
   let count = 0;
-  const recursive = (num, loc) => {
-    for (let i = loc; i < n; i++) {
+  const recursive = (num) => {
+    for (let i = 0; i < n; i++) {
       if (computers[num][i] === 1) {
         computers[num][i] = 0;
-        if (num !== i) recursive(i, 0);
+        if (num !== i) recursive(i);
       }
     }
   };
   for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
+    for (let j = i; j < n; j++) {
       if (computers[i][j] === 1) {
-        recursive(i, 0);
+        recursive(i);
         count++;
       }
     }
